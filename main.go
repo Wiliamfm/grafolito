78b43336a27ea632dcabf6723d59a3584230d7a8
william <santifon15@gmail.com>
2025-04-19 20:19:19 -0500

    add identity apis

--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"log"
 	"net/http"
+	identity "grafolito/backend/identity/api"
 )
 
 func main() {
+
+	identity.MapEndpoints()
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Not Found!"))
